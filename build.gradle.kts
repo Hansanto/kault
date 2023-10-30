d650ba3a4e4fb6de35b4388801e569b9f9ddb00c
Hansanto <anthony.hanson@outlook.fr>
2023-10-30 22:25:18 +0100

    feat: Init project

---
+++ build.gradle.kts
@@ -0,0 +1,112 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+
+plugins {
+    embeddedKotlin("multiplatform")
+    id("org.jetbrains.dokka") version "1.9.10"
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
+}
+
+repositories {
+    mavenCentral()
+}
+
+detekt {
+    val envIgnoreFailures = System.getenv("DETEKT_IGNORE_FAILURES")?.toBooleanStrictOrNull() ?: false
+    ignoreFailures = envIgnoreFailures
+    config.from(file("config/detekt/detekt.yml"))
+}
+
+
+kotlin {
+    explicitApi = org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode.Strict
+
+    jvm {
+        jvmToolchain(8)
+        withJava()
+        testRuns.named("test") {
+            executionTask.configure {
+                useJUnitPlatform()
+            }
+        }
+    }
+    js {
+        nodejs()
+    }
+
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    sourceSets {
+
+        val commonMain by getting {
+        }
+        val commonTest by getting {
+
+            val kotestVersion = "5.7.2"
+
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("io.kotest:kotest-assertions-core:$kotestVersion")
+            }
+        }
+
+        val jvmMain by getting {
+            dependsOn(commonMain)
+        }
+        val jvmTest by getting
+
+        val jsMain by getting {
+            dependsOn(commonMain)
+        }
+        val jsTest by getting
+
+        val nativeMain by getting {
+            dependsOn(commonMain)
+        }
+        val nativeTest by getting
+    }
+}
+
+val dokkaOutputDir = "${rootProject.projectDir}/dokka"
+
+tasks {
+    withType<Detekt>().configureEach {
+        jvmTarget = "1.8"
+
+        reports {
+            html.required.set(true)
+            xml.required.set(true)
+            txt.required.set(false)
+            sarif.required.set(false)
+            md.required.set(false)
+        }
+    }
+
+    withType<DetektCreateBaselineTask>().configureEach {
+        jvmTarget = "1.8"
+    }
+
+    clean {
+        delete(dokkaOutputDir)
+    }
+
+    val deleteDokkaOutputDir by register<Delete>("deleteDokkaOutputDirectory") {
+        group = "documentation"
+        delete(dokkaOutputDir)
+    }
+
+    dokkaHtml.configure {
+        dependsOn(deleteDokkaOutputDir)
+        outputDirectory.set(file(dokkaOutputDir))
+    }
+}
