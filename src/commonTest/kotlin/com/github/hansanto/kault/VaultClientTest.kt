8ee07b97e18aca38b492da5fe2f882979168758d
Hansanto <anthony.hanson@outlook.fr>
2023-11-01 12:36:41 +0100

    test: Remove outdated code

--- src/commonTest/kotlin/com/github/hansanto/kault/VaultClientTest.kt
+++ src/commonTest/kotlin/com/github/hansanto/kault/VaultClientTest.kt
@@ -1,12 +1,3 @@
 package com.github.hansanto.kault
 
-import io.kotest.matchers.shouldBe
-import kotlin.test.Test
-
-class VaultClientTest {
-
-    @Test
-    fun shouldDoSomething() {
-        VaultClient().doSomething() shouldBe "VaultClient"
-    }
-}
+class VaultClientTest
