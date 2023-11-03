b1fcb1665f5754c44b577a4ce050ce832ebe6732
Hansanto <anthony.hanson@outlook.fr>
2023-11-03 19:41:17 +0100

    test(approle): Continue tests

--- src/commonTest/kotlin/com/github/hansanto/kault/auth/approle/VaultAuthAppRoleTest.kt
+++ src/commonTest/kotlin/com/github/hansanto/kault/auth/approle/VaultAuthAppRoleTest.kt
@@ -10,6 +10,8 @@ import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlinx.coroutines.flow.toList
 
 private const val DEFAULT_ROLE_NAME = "test"
 
@@ -42,16 +44,51 @@ class VaultAuthAppRoleTest : FunSpec({
         shouldThrow<VaultAPIException> { appRole.read(DEFAULT_ROLE_NAME) }
     }
 
-    xtest("create without options") {
+    test("list with no roles") {
+        shouldThrow<VaultAPIException> {
+            appRole.list().toList()
+        }
+    }
+
+    test("list with roles") {
+        val roles = List(10) { "test-$it" }
+        roles.forEach { appRole.createOrUpdate(it) shouldBe true }
+        appRole.list().toList() shouldBe roles
+    }
+
+    test("create without options and read") {
         appRole.createOrUpdate(DEFAULT_ROLE_NAME) shouldBe true
         shouldNotThrow<VaultAPIException> { appRole.read(DEFAULT_ROLE_NAME) }
     }
 
-    test("create with options") {
+    test("create with options and read") {
         val given = readJson<CreateOrUpdatePayload>("cases/auth/approle/create/given.json")
         appRole.createOrUpdate(DEFAULT_ROLE_NAME, given) shouldBe true
 
         val expected = readJson<AppRoleReadRoleResponse>("cases/auth/approle/create/expected.json")
         appRole.read(DEFAULT_ROLE_NAME) shouldBe expected
+    }
+
+    test("delete non-existing role") {
+        shouldThrow<VaultAPIException> { appRole.read(DEFAULT_ROLE_NAME) }
+        appRole.delete(DEFAULT_ROLE_NAME) shouldBe true
+        shouldThrow<VaultAPIException> { appRole.read(DEFAULT_ROLE_NAME) }
+    }
+
+    test("delete existing role") {
+        appRole.createOrUpdate(DEFAULT_ROLE_NAME) shouldBe true
+        shouldNotThrow<VaultAPIException> { appRole.read(DEFAULT_ROLE_NAME) }
+        appRole.delete(DEFAULT_ROLE_NAME) shouldBe true
+        shouldThrow<VaultAPIException> { appRole.read(DEFAULT_ROLE_NAME) }
+    }
+
+    test("read non-existing roleId") {
+        shouldThrow<VaultAPIException> { appRole.readRoleID(DEFAULT_ROLE_NAME) }
+    }
+
+    test("read existing roleId") {
+        appRole.createOrUpdate(DEFAULT_ROLE_NAME) shouldBe true
+        val response = appRole.readRoleID(DEFAULT_ROLE_NAME)
+        response.roleId.length shouldNotBe 0
     }
 })
