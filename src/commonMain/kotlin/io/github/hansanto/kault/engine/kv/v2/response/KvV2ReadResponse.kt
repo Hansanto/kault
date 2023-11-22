12d5d415531ceef04abd4af022659123ffa0f9c0
Hansanto <anthony.hanson@outlook.fr>
2023-11-22 22:20:38 +0100

    fix: Move Metadata as common data class

--- src/commonMain/kotlin/io/github/hansanto/kault/engine/kv/v2/response/KvV2ReadResponse.kt
+++ src/commonMain/kotlin/io/github/hansanto/kault/engine/kv/v2/response/KvV2ReadResponse.kt
@@ -1,7 +1,7 @@
 package io.github.hansanto.kault.engine.kv.v2.response
 
 import io.github.hansanto.kault.VaultClient
-import kotlinx.datetime.Instant
+import io.github.hansanto.kault.common.Metadata
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -14,7 +14,7 @@ public data class KvV2ReadResponse(
 
     /**
      * The key-value pair data.
-     * The value has arbitrary structure, encoded using JSON.
+     * The value has an arbitrary structure, encoded using JSON.
      */
     @SerialName("data")
     val data: JsonObject,
@@ -26,51 +26,6 @@ public data class KvV2ReadResponse(
     val metadata: Metadata
 
 ) {
-    @Serializable
-    public data class Metadata(
-
-        /**
-         * The time at which the version was created.
-         */
-        @SerialName("created_time")
-        val createdTime: Instant,
-
-        /**
-         * The custom metadata.
-         */
-        @SerialName("custom_metadata")
-        val customMetadata: JsonObject?,
-
-        /**
-         * The time at which the version was deleted.
-         */
-        @SerialName("deletion_time")
-        val deletionTime: Instant?,
-
-        /**
-         * True if the version is destroyed.
-         */
-        @SerialName("destroyed")
-        val destroyed: Boolean,
-
-        /**
-         * Version of the secret.
-         */
-        @SerialName("version")
-        val version: Int
-    ) {
-
-        /**
-         * Decode the [customMetadata] to [T] using [format] and [serializer].
-         * @param format Format to use to decode the [customMetadata] field.
-         * @param serializer Serializer to indicate the strategy to decode the [customMetadata] field.
-         * @return The decoded [customMetadata] field.
-         */
-        public inline fun <reified T> customMetadata(
-            format: Json = VaultClient.json,
-            serializer: KSerializer<T> = format.serializersModule.serializer<T>()
-        ): T? = customMetadata?.let { format.decodeFromJsonElement(serializer, it) }
-    }
 
     /**
      * Decode the [data] to [T] using [format] and [serializer].
