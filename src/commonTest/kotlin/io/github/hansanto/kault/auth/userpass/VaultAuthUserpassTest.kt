d3609f57110722d3f9ca4cdff001b3c4a6ed3bfd
Hansanto <anthony.hanson@outlook.fr>
2023-12-22 18:05:52 +0100

    test: Implement test for userpass

--- src/commonTest/kotlin/io/github/hansanto/kault/auth/userpass/VaultAuthUserpassTest.kt
+++ src/commonTest/kotlin/io/github/hansanto/kault/auth/userpass/VaultAuthUserpassTest.kt
@@ -3,11 +3,15 @@ package io.github.hansanto.kault.auth.userpass
 import io.github.hansanto.kault.VaultClient
 import io.github.hansanto.kault.auth.userpass.payload.UserpassWriteUserPayload
 import io.github.hansanto.kault.auth.userpass.response.ReadUserResponse
+import io.github.hansanto.kault.exception.VaultAPIException
 import io.github.hansanto.kault.system.auth.enable
 import io.github.hansanto.kault.util.createVaultClient
 import io.github.hansanto.kault.util.randomString
 import io.github.hansanto.kault.util.readJson
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.shouldBe
 
 class VaultAuthUserpassTest : FunSpec({
@@ -30,6 +34,14 @@ class VaultAuthUserpassTest : FunSpec({
         client.close()
     }
 
+    beforeTest {
+        runCatching {
+            userpass.list()
+        }.getOrNull()?.forEach {
+            userpass.delete(it)
+        }
+    }
+
     test("create without options") {
         assertCreate(
             userpass,
@@ -44,6 +56,106 @@ class VaultAuthUserpassTest : FunSpec({
             "cases/auth/userpass/create/with_options/given.json",
             "cases/auth/userpass/create/with_options/expected.json"
         )
+    }
+
+    test("delete with non existing user") {
+        val username = randomString()
+        userpass.delete(username) shouldBe true
+
+        shouldThrow<VaultAPIException> {
+            userpass.read(username)
+        }
+    }
+
+    test("delete with existing user") {
+        val username = randomString()
+        userpass.createOrUpdate(username) {
+            password = randomString()
+        }
+
+        shouldNotThrow<VaultAPIException> {
+            userpass.read(username)
+        }
+
+        userpass.delete(username) shouldBe true
+
+        shouldThrow<VaultAPIException> {
+            userpass.read(username)
+        }
+    }
+
+    test("update password with non existing user") {
+        val username = randomString()
+        val password = randomString()
+        shouldThrow<VaultAPIException> {
+            userpass.updatePassword(username, password)
+        }
+    }
+
+    test("update password with existing user") {
+        val username = randomString()
+        val firstPassword = randomString()
+        val secondPassword = randomString()
+        userpass.createOrUpdate(username) {
+            password = firstPassword
+        }
+
+        shouldNotThrow<VaultAPIException> {
+            userpass.login(username, firstPassword)
+        }
+
+        shouldThrow<VaultAPIException> {
+            userpass.login(username, secondPassword)
+        }
+
+        userpass.updatePassword(username, secondPassword) shouldBe true
+
+        shouldThrow<VaultAPIException> {
+            userpass.login(username, firstPassword)
+        }
+
+        shouldNotThrow<VaultAPIException> {
+            userpass.login(username, secondPassword)
+        }
+    }
+
+    test("update policies with non existing user") {
+        val username = randomString()
+        val policies = listOf(randomString())
+        shouldThrow<VaultAPIException> {
+            userpass.updatePolicies(username, policies)
+        }
+    }
+
+    test("update policies with existing user") {
+        val username = randomString()
+        val initialPolicies = List(3) { randomString() }
+        val newPolicies = listOf(randomString())
+        userpass.createOrUpdate(username) {
+            password = randomString()
+            tokenPolicies = initialPolicies
+        }
+
+        userpass.updatePolicies(username, newPolicies) shouldBe true
+
+        val user = userpass.read(username)
+        user.tokenPolicies shouldBe newPolicies
+    }
+
+    test("list with no users") {
+        shouldThrow<VaultAPIException> {
+            userpass.list()
+        }
+    }
+
+    test("list with users") {
+        val users = List(3) { randomString() }
+        users.forEach {
+            userpass.createOrUpdate(it) {
+                password = randomString()
+            }
+        }
+        userpass.list() shouldContainExactlyInAnyOrder users
     }
 })
 
