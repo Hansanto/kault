43d37005a4821cef95457c01c4fdbd5c6f2730ce
Hansanto <anthony.hanson@outlook.fr>
2024-03-04 20:59:06 +0100

    test: Add util file documentation

--- src/commonTest/kotlin/io/github/hansanto/kault/util/KubernetesUtil.kt
+++ src/commonTest/kotlin/io/github/hansanto/kault/util/KubernetesUtil.kt
@@ -3,22 +3,65 @@ package io.github.hansanto.kault.util
 import io.kotest.mpp.env
 import kotlinx.io.files.Path
 
+/**
+ * Environment variable name to retrieve IP of minikube.
+ */
 private const val MINIKUBE_IP_ENV = "MINIKUBE_IP"
+
+/**
+ * Environment variable name to retrieve port of minikube.
+ */
+private const val MINIKUBE_PORT_ENV = "MINIKUBE_PORT"
+
+/**
+ * Default port of minikube, check [documentation](https://minikube.sigs.k8s.io/docs/commands/start/).
+ */
+private const val MINIKUBE_DEFAULT_PORT = 8443
+
+/**
+ * Folder name of minikube in user's home directory.
+ */
 private const val MINIKUBE_FOLDER = ".minikube"
+
+/**
+ * File name of CA certificate in minikube folder.
+ */
 private const val CA_CERT_FILE = "ca.crt"
 
-fun getKubernetesHost(): String {
-    return "http://${getMinikubeIp()}:8443"
+/**
+ * Get the kubernetes host URL.
+ * @param port Port of minikube, default is [MINIKUBE_DEFAULT_PORT].
+ * @return URL of kubernetes host.
+ */
+fun getKubernetesHost(port: Int = env(MINIKUBE_PORT_ENV)?.toInt() ?: MINIKUBE_DEFAULT_PORT): String {
+    return "http://${getKubernetesIp()}:$port"
 }
 
-fun getMinikubeIp(): String {
+/**
+ * Get the IP of minikube from the environment variable [MINIKUBE_IP_ENV].
+ * @return IP of minikube.
+ */
+fun getKubernetesIp(): String {
     return env(MINIKUBE_IP_ENV) ?: throw IllegalStateException("$MINIKUBE_IP_ENV environment variable must be set")
 }
 
+/**
+ * Get the CA certificate of minikube.
+ * Walks from the project directory to the user's home directory
+ * to find the minikube folder and the CA certificate file.
+ * @return CA certificate of minikube in string format.
+ */
 fun getKubernetesCaCert(): String {
     return getKubernetesCaCertPath().readLines()
 }
 
+/**
+ * Get the CA certificate path of minikube.
+ * Walks from the project directory to the user's home directory
+ * to find the minikube folder and the CA certificate file.
+ * If the file is not found, an exception will be thrown.
+ * @return Path of CA certificate of minikube.
+ */
 fun getKubernetesCaCertPath(): Path {
     val minikubeFolderPath = findFolderInParent(MINIKUBE_FOLDER)
     require(minikubeFolderPath != null && minikubeFolderPath.exists()) { "$MINIKUBE_FOLDER folder not found" }
