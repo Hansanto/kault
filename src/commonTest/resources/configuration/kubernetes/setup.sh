6d49133b05c6a581b383337c2fe04c47bc5d4932
Hansanto <anthony.hanson@outlook.fr>
2024-03-10 18:33:39 +0100

    test: Generate token from setup

--- src/commonTest/resources/configuration/kubernetes/setup.sh
+++ src/commonTest/resources/configuration/kubernetes/setup.sh
@@ -3,6 +3,12 @@
 # Change the current directory during the execution of this script to retrieve relative files
 cd "$(dirname "$0")" || exit 1
 
-kubectl create namespace kault-test
-kubectl create serviceaccount vault -n kault-test
+SERVICE_ACCOUNT="vault"
+NAMESPACE="kault-test"
+
+kubectl create namespace $NAMESPACE
+kubectl create serviceaccount $SERVICE_ACCOUNT -n $NAMESPACE
 kubectl apply -f permissions.yml
+
+KUBERNETES_TOKEN=$(kubectl create token $SERVICE_ACCOUNT -n $NAMESPACE)
+echo "$KUBERNETES_TOKEN"
