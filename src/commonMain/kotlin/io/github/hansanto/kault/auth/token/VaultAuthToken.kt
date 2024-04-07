a13571f57e1be4c50e6385f73cd3fde331d7123a
Hansanto <anthony.hanson@outlook.fr>
2024-04-07 17:24:13 +0200

    feat: Add interface token auth

--- src/commonMain/kotlin/io/github/hansanto/kault/auth/token/VaultAuthToken.kt
+++ src/commonMain/kotlin/io/github/hansanto/kault/auth/token/VaultAuthToken.kt
@@ -3,6 +3,7 @@ package io.github.hansanto.kault.auth.token
 import io.github.hansanto.kault.BuilderDsl
 import io.github.hansanto.kault.ServiceBuilder
 import io.github.hansanto.kault.auth.VaultAuth
+import io.github.hansanto.kault.serializer.VaultDuration
 import io.ktor.client.HttpClient
 
 public interface VaultAuthToken {
@@ -35,6 +36,129 @@ public interface VaultAuthToken {
      * @return Any
      */
     public suspend fun lookupSelfToken(): Any
+
+    /**
+     * Returns information about the client token from the accessor.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#lookup-a-token-accessor)
+     * @param accessor Token accessor to lookup.
+     * @return Any
+     */
+    public suspend fun lookupAccessorToken(accessor: String): Any
+
+    /**
+     * Renews a lease associated with a token.
+     * This is used to prevent the expiration of a token, and the automatic revocation of it.
+     * Token renewal is possible only if there is a lease associated with it.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#renew-a-token)
+     * @param payload Any
+     * @return Any
+     */
+    public suspend fun renewToken(payload: Any): Any
+
+    /**
+     * Renews a lease associated with the calling token.
+     * This is used to prevent the expiration of a token, and the automatic revocation of it.
+     * Token renewal is possible only if there is a lease associated with it.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#renew-a-token-self)
+     * @param increment An optional requested increment duration can be provided.
+     * This increment may not be honored, for instance in the case of periodic tokens.
+     * If not supplied, Vault will use the default TTL.
+     * @return Any
+     */
+    public suspend fun renewSelfToken(increment: VaultDuration): Any
+
+    /**
+     * Renews a lease associated with a token using its accessor.
+     * This is used to prevent the expiration of a token, and the automatic revocation of it.
+     * Token renewal is possible only if there is a lease associated with it.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#renew-a-token-accessor)
+     * @param payload Any
+     * @return Any
+     */
+    public suspend fun renewAccessorToken(payload: Any): Any
+
+    /**
+     * Revokes a token and all child tokens.
+     * When the token is revoked, all dynamic secrets generated with it are also revoked.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#revoke-a-token)
+     * @param token Token to revoke.
+     * @return Any
+     */
+    public suspend fun revokeToken(token: String): Any
+
+    /**
+     * Revokes the token used to call it and all child tokens.
+     * When the token is revoked, all dynamic secrets generated with it are also revoked.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#revoke-a-token-self)
+     * @return Any
+     */
+    public suspend fun revokeSelfToken(): Any
+
+    /**
+     * Revoke the token associated with the accessor and all the child tokens.
+     * This is meant for purposes where there is no access to token ID
+     * but there is need to revoke a token and its children.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#revoke-a-token-accessor)
+     * @param accessor Accessor of the token.
+     * @return Any
+     */
+    public suspend fun revokeAccessorToken(accessor: String): Any
+
+    /**
+     * Revokes a token but not its child tokens.
+     * When the token is revoked, all secrets generated with it are also revoked.
+     * All child tokens are orphaned, but can be revoked sub-sequently using /auth/token/revoke/.
+     * This is a root-protected endpoint.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#revoke-token-and-orphan-children)
+     * @param token Token to revoke. This can be part of the URL or the body.
+     * @return Any
+     */
+    public suspend fun revokeTokenAndOrphan(token: String): Any
+
+    /**
+     * Fetches the named role configuration.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#read-token-role)
+     * @param roleName The name of the token role.
+     * @return Any
+     */
+    public suspend fun readTokenRole(roleName: String): Any
+
+    /**
+     * List available token roles.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#list-token-roles)
+     * @return Any
+     */
+    public suspend fun listTokenRoles(): Any
+
+    /**
+     * Creates (or replaces) the named role.
+     * Roles enforce specific behavior
+     * when creating tokens that allow token functionality
+     * that is otherwise not available or would require sudo/root privileges to access.
+     * Role parameters, when set, override any provided options to the create endpoints.
+     * The role name is also included in the token path,
+     * allowing all tokens created against a role to be revoked using the /sys/leases/revoke-prefix endpoint.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#create-update-token-role)
+     * @param roleName The name of the token role.
+     * @param payload Any
+     * @return Any
+     */
+    public suspend fun createOrUpdateTokenRole(roleName: String, payload: Any): Any
+
+    /**
+     * This endpoint deletes the named token role.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#delete-token-role)
+     * @param roleName The name of the token role.
+     * @return Any
+     */
+    public suspend fun deleteTokenRole(roleName: String): Any
+
+    /**
+     * Performs some maintenance tasks to clean up invalid entries that may remain in the token store. On Enterprise, Tidy will only impact the tokens in the specified namespace, or the root namespace if unspecified.
+     * [Documentation](https://developer.hashicorp.com/vault/api-docs/auth/token#tidy-tokens)
+     * @return Any
+     */
+    public suspend fun tidyTokens(): Any
 }
 
 /**
@@ -87,19 +211,4 @@ public class VaultAuthTokenImpl(
         }
     }
 
-    override suspend fun listAccessors(): List<String> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun createToken(payload: Any): Any {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun lookupToken(token: String): Any {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun lookupSelfToken(): Any {
-        TODO("Not yet implemented")
-    }
 }
