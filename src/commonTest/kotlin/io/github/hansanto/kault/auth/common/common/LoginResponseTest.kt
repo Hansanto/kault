62482d75be565f95458747d07d03e39ec10cdf12
Hansanto <anthony.hanson@outlook.fr>
2024-04-16 18:02:52 +0200

    test: Add mapper tests

---
+++ src/commonTest/kotlin/io/github/hansanto/kault/auth/common/common/LoginResponseTest.kt
@@ -0,0 +1,42 @@
+package io.github.hansanto.kault.auth.common.common
+
+import io.github.hansanto.kault.auth.common.response.LoginResponse
+import io.github.hansanto.kault.auth.common.response.toTokenInfo
+import io.github.hansanto.kault.util.randomBoolean
+import io.github.hansanto.kault.util.randomLong
+import io.github.hansanto.kault.util.randomString
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+import kotlin.time.Duration.Companion.milliseconds
+
+class LoginResponseTest : ShouldSpec({
+
+    should("transform LoginResponse to TokenInfo") {
+        val response = LoginResponse(
+            clientToken = randomString(),
+            accessor = randomString(),
+            tokenPolicies = List(5) { randomString() },
+            metadata = mapOf(randomString() to randomString()),
+            leaseDuration = randomLong().milliseconds,
+            renewable = randomBoolean(),
+            entityId = randomString(),
+            tokenType = TokenType.entries.random(),
+            orphan = randomBoolean(),
+            numUses = randomLong()
+        )
+
+        val toTokenInfo = response.toTokenInfo()
+        toTokenInfo shouldBe TokenInfo(
+            token = response.clientToken,
+            accessor = response.accessor,
+            tokenPolicies = response.tokenPolicies,
+            metadata = response.metadata!!,
+            expirationDate = toTokenInfo.expirationDate,
+            renewable = response.renewable,
+            entityId = response.entityId,
+            tokenType = response.tokenType,
+            orphan = response.orphan,
+            numUses = response.numUses
+        )
+    }
+})
---
+++ src/commonTest/kotlin/io/github/hansanto/kault/auth/token/TokenRenewResponseTest.kt
@@ -0,0 +1,45 @@
+package io.github.hansanto.kault.auth.token
+
+import io.github.hansanto.kault.auth.common.common.TokenInfo
+import io.github.hansanto.kault.auth.common.common.TokenType
+import io.github.hansanto.kault.auth.token.response.TokenRenewResponse
+import io.github.hansanto.kault.auth.token.response.toTokenInfo
+import io.github.hansanto.kault.util.randomBoolean
+import io.github.hansanto.kault.util.randomLong
+import io.github.hansanto.kault.util.randomString
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+import kotlin.time.Duration.Companion.milliseconds
+
+class TokenRenewResponseTest : ShouldSpec({
+
+    should("transform TokenRenewResponse to TokenInfo") {
+        val response = TokenRenewResponse(
+            clientToken = randomString(),
+            accessor = randomString(),
+            policies = List(5) { randomString() },
+            tokenPolicies = List(5) { randomString() },
+            metadata = mapOf(randomString() to randomString()),
+            leaseDuration = randomLong().milliseconds,
+            renewable = randomBoolean(),
+            entityId = randomString(),
+            tokenType = TokenType.entries.random(),
+            orphan = randomBoolean(),
+            numUses = randomLong()
+        )
+
+        val toTokenInfo = response.toTokenInfo()
+        toTokenInfo shouldBe TokenInfo(
+            token = response.clientToken,
+            accessor = response.accessor,
+            tokenPolicies = response.tokenPolicies,
+            metadata = response.metadata!!,
+            expirationDate = toTokenInfo.expirationDate,
+            renewable = response.renewable,
+            entityId = response.entityId,
+            tokenType = response.tokenType,
+            orphan = response.orphan,
+            numUses = response.numUses
+        )
+    }
+})
