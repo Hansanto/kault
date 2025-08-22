d5b5b303317931aa9fce76b9f1b0d491d81c2524
Hansanto <anthony.hanson@outlook.fr>
2025-08-22 19:51:47 +0200

    test: Replace deprecated functions

--- src/commonTest/kotlin/io/github/hansanto/kault/util/matcher/InstantMatcher.kt
+++ src/commonTest/kotlin/io/github/hansanto/kault/util/matcher/InstantMatcher.kt
@@ -1,6 +1,6 @@
 package io.github.hansanto.kault.util.matcher
 
-import io.kotest.matchers.longs.between
+import io.kotest.matchers.comparables.between
 import io.kotest.matchers.shouldBe
 import kotlin.time.Instant
 
