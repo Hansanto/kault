8b6915caee9cd26c42ad81d3f2dbc97c1d8f6542
Hansanto <anthony.hanson@outlook.fr>
2026-02-28 06:57:19 +0100

    test: Continue implement tests

--- src/commonTest/kotlin/io/github/hansanto/kault/identity/entity/VaultIdentityEntityTest.kt
+++ src/commonTest/kotlin/io/github/hansanto/kault/identity/entity/VaultIdentityEntityTest.kt
@@ -2,10 +2,12 @@ package io.github.hansanto.kault.identity.entity
 
 import io.github.hansanto.kault.VaultClient
 import io.github.hansanto.kault.exception.VaultAPIException
+import io.github.hansanto.kault.identity.entity.payload.EntityCreateOrUpdateByNamePayload
 import io.github.hansanto.kault.identity.entity.payload.EntityCreateOrUpdatePayload
 import io.github.hansanto.kault.identity.entity.payload.EntityUpdateByIDPayload
 import io.github.hansanto.kault.identity.entity.response.EntityCreateResponse
 import io.github.hansanto.kault.identity.entity.response.EntityReadResponse
+import io.github.hansanto.kault.util.DEFAULT_ROLE_NAME
 import io.github.hansanto.kault.util.createVaultClient
 import io.github.hansanto.kault.util.randomString
 import io.github.hansanto.kault.util.readJson
@@ -33,7 +35,7 @@ class VaultIdentityEntityTest : ShouldSpec({
         client.close()
     }
 
-    should("use default path if not set in builder") {
+    xshould("use default path if not set in builder") {
         VaultIdentityEntityImpl.Default.PATH shouldBe "entity"
 
         val built = VaultIdentityEntityImpl.Companion(client.client, null) {
@@ -42,7 +44,7 @@ class VaultIdentityEntityTest : ShouldSpec({
         built.path shouldBe VaultIdentityEntityImpl.Default.PATH
     }
 
-    should("use custom path if set in builder") {
+    xshould("use custom path if set in builder") {
         val builderPath = randomString()
         val parentPath = randomString()
 
@@ -53,7 +55,7 @@ class VaultIdentityEntityTest : ShouldSpec({
         built.path shouldBe "$parentPath/$builderPath"
     }
 
-    should("create an entity with default values") {
+    xshould("create an entity with default values") {
         assertCreateEntity(
             identityEntity,
             null,
@@ -61,7 +63,7 @@ class VaultIdentityEntityTest : ShouldSpec({
         )
     }
 
-    should("create an entity with all defined values") {
+    xshould("create an entity with all defined values") {
         assertCreateEntity(
             identityEntity,
             "cases/identity/entity/create/with_options/given.json",
@@ -69,7 +71,7 @@ class VaultIdentityEntityTest : ShouldSpec({
         )
     }
 
-    should("create an entity using builder with default values") {
+    xshould("create an entity using builder with default values") {
         assertCreateEntityWithBuilder(
             identityEntity,
             null,
@@ -77,7 +79,7 @@ class VaultIdentityEntityTest : ShouldSpec({
         )
     }
 
-    should("create an entity using builder with all defined values") {
+    xshould("create an entity using builder with all defined values") {
         assertCreateEntityWithBuilder(
             identityEntity,
             "cases/identity/entity/create/with_options/given.json",
@@ -85,7 +87,7 @@ class VaultIdentityEntityTest : ShouldSpec({
         )
     }
 
-    should("update an entity if it exists") {
+    xshould("update an entity if it exists") {
         val given = readJson<EntityCreateOrUpdatePayload>("cases/identity/entity/update/given_create.json")
         val created = identityEntity.createOrUpdateEntity(given)
         created shouldNotBe null
@@ -102,13 +104,13 @@ class VaultIdentityEntityTest : ShouldSpec({
         ) shouldBe actual
     }
 
-    should("throw exception when reading a non-existing entity by id") {
-        shouldThrow< VaultAPIException> {
+    xshould("throw exception when reading a non-existing entity by id") {
+        shouldThrow<VaultAPIException> {
             identityEntity.readEntityByID("non-existing-id")
         }
     }
 
-    should("read existing entity by id") {
+    xshould("read existing entity by id") {
         val response = identityEntity.createOrUpdateEntity()
         response shouldNotBe null
 
@@ -117,13 +119,13 @@ class VaultIdentityEntityTest : ShouldSpec({
         actual.name shouldBe response.name
     }
 
-    should("throw exception when updating a non-existing entity by id") {
+    xshould("throw exception when updating a non-existing entity by id") {
         shouldThrow<VaultAPIException> {
             identityEntity.updateEntityByID("non-existing-id")
         }
     }
 
-    should("update an existing entity by id") {
+    xshould("update an existing entity by id") {
         val given = readJson<EntityCreateOrUpdatePayload>("cases/identity/entity/update_by_id/given_create.json")
         val created = identityEntity.createOrUpdateEntity(given)
         created shouldNotBe null
@@ -139,14 +141,14 @@ class VaultIdentityEntityTest : ShouldSpec({
         ) shouldBe actual
     }
 
-    should("return true when deleting a non-existing entity by id") {
+    xshould("return true when deleting a non-existing entity by id") {
         val id = "non-existing-id"
         shouldThrow<VaultAPIException> { identityEntity.readEntityByID(id) }
         identityEntity.deleteEntityByID(id) shouldBe true
         shouldThrow<VaultAPIException> { identityEntity.readEntityByID(id) }
     }
 
-    should("delete existing entity by id") {
+    xshould("delete existing entity by id") {
         val response = identityEntity.createOrUpdateEntity()
         val id = response!!.id
         shouldNotThrow<VaultAPIException> { identityEntity.readEntityByID(id) }
@@ -154,14 +156,14 @@ class VaultIdentityEntityTest : ShouldSpec({
         shouldThrow<VaultAPIException> { identityEntity.readEntityByID(id) }
     }
 
-    should("return true when deleting a non-existing entities by batch") {
+    xshould("return true when deleting a non-existing entities by batch") {
         val ids = listOf("non-existing-id-1", "non-existing-id-2", "non-existing-id-3")
         ids.forEach { shouldThrow<VaultAPIException> { identityEntity.readEntityByID(it) } }
         identityEntity.batchDeleteEntities(ids) shouldBe true
         ids.forEach { shouldThrow<VaultAPIException> { identityEntity.readEntityByID(it) } }
     }
 
-    should("return true when deleting partial existing entities by batch") {
+    xshould("return true when deleting partial existing entities by batch") {
         val existingIds = List(3) {
             val response = identityEntity.createOrUpdateEntity()
             response!!.id
@@ -173,7 +175,7 @@ class VaultIdentityEntityTest : ShouldSpec({
         allIds.forEach { shouldThrow<VaultAPIException> { identityEntity.readEntityByID(it) } }
     }
 
-    should("delete existing entities by batch") {
+    xshould("delete existing entities by batch") {
         val ids = List(3) {
             val response = identityEntity.createOrUpdateEntity()
             response!!.id
@@ -183,13 +185,13 @@ class VaultIdentityEntityTest : ShouldSpec({
         ids.forEach { shouldThrow<VaultAPIException> { identityEntity.readEntityByID(it) } }
     }
 
-    should("throw exception when listing the ids of the entities with no entities") {
+    xshould("throw exception when listing the ids of the entities with no entities") {
         shouldThrow<VaultAPIException> {
             identityEntity.listEntitiesByID()
         }
     }
 
-    should("return list of ids from existing entities") {
+    xshould("return list of ids from existing entities") {
         val createdIds = List(3) {
             identityEntity.createOrUpdateEntity()!!
         }
@@ -199,48 +201,103 @@ class VaultIdentityEntityTest : ShouldSpec({
         listEntitiesByID.keyInfo.values.map { it.name } shouldContainExactlyInAnyOrder createdIds.map { it.name }
     }
 
-    should("create an entity by name with default values") {
-        TODO()
+    xshould("create an entity by name with default values") {
+        assertCreateEntityByName(
+            "create_entity_name_by_name",
+            identityEntity,
+            null,
+            "cases/identity/entity/create_by_name/without_options/expected.json"
+        )
     }
 
-    should("create an entity by name with all defined values") {
-        TODO()
+    xshould("create an entity by name with all defined values") {
+        assertCreateEntityByName(
+            "create_entity_name_by_name",
+            identityEntity,
+            "cases/identity/entity/create_by_name/with_options/given.json",
+            "cases/identity/entity/create_by_name/without_options/expected.json"
+        )
     }
 
-    should("create an entity by name using builder with default values") {
-        TODO()
+    xshould("create an entity by name using builder with default values") {
+        assertCreateEntityByNameWithBuilder(
+            "create_entity_name_by_name",
+            identityEntity,
+            null,
+            "cases/identity/entity/create_by_name/without_options/expected.json"
+        )
     }
 
-    should("create an entity by name using builder with all defined values") {
-        TODO()
+    xshould("create an entity by name using builder with all defined values") {
+        assertCreateEntityByNameWithBuilder(
+            "create_entity_name_by_name",
+            identityEntity,
+            "cases/identity/entity/create_by_name/with_options/given.json",
+            "cases/identity/entity/create_by_name/without_options/expected.json"
+        )
     }
 
-    should("update an entity by name if it exists") {
-        TODO()
+    xshould("update an entity by name if it exists") {
+        val name = "update_entity_by_name"
+        val given = readJson<EntityCreateOrUpdateByNamePayload>("cases/identity/entity/update_by_name/given_create.json")
+        val created = identityEntity.createOrUpdateEntityByName(name, given)
+        created shouldNotBe null
+
+        val updatePayload = readJson<EntityCreateOrUpdateByNamePayload>("cases/identity/entity/update_by_name/given_update.json")
+        val updated = identityEntity.createOrUpdateEntityByName(name, updatePayload)
+        updated shouldBe null
+
+        val actual = identityEntity.readEntityByName(name)
+        replaceTemplateString(
+            expected = readJson<EntityReadResponse>("cases/identity/entity/update_by_name/expected.json"),
+            response = actual,
+        ) shouldBe actual
     }
 
-    should("throw exception when reading a non-existing entity by name") {
-        TODO()
+    xshould("throw exception when reading a non-existing entity by name") {
+        shouldThrow<VaultAPIException> {
+            identityEntity.readEntityByName("non-existing-name")
+        }
     }
 
-    should("return entity info when reading an existing entity by name") {
-        TODO()
+    xshould("read existing entity by name") {
+        val name = "read_entity_by_name"
+        val response = identityEntity.createOrUpdateEntityByName(name)
+        response shouldNotBe null
+
+        val actual = identityEntity.readEntityByName(name)
+        actual.id shouldBe response!!.id
+        actual.name shouldBe response.name
     }
 
-    should("return true when deleting a non-existing entity by name") {
-        TODO()
+    xshould("return true when deleting a non-existing entity by name") {
+        val name = "delete_non_existing_entity_by_name"
+        shouldThrow<VaultAPIException> { identityEntity.readEntityByName(name) }
+        identityEntity.deleteEntityByName(name) shouldBe true
+        shouldThrow<VaultAPIException> { identityEntity.readEntityByName(name) }
     }
 
-    should("delete an existing entity by name") {
-        TODO()
+    xshould("delete existing entity by name") {
+        val name = "delete_entity_by_name"
+        val response = identityEntity.createOrUpdateEntityByName(name)
+        val id = response!!.id
+        shouldNotThrow<VaultAPIException> { identityEntity.readEntityByID(id) }
+        identityEntity.deleteEntityByName(name) shouldBe true
+        shouldThrow<VaultAPIException> { identityEntity.readEntityByID(id) }
     }
 
-    should("throw exception when listing the names of the entities with no entities") {
-        TODO()
+    xshould("throw exception when listing the names of the entities with no entities") {
+        shouldThrow<VaultAPIException> {
+            identityEntity.listEntitiesByName()
+        }
     }
 
-    should("return list of names from existing entities") {
-        TODO()
+    xshould("return list of names from existing entities") {
+        val expectedListName = List(3) { "merge_non_existing_source_entity_$it" }
+        expectedListName.forEach { identityEntity.createOrUpdateEntityByName(it) }
+
+        val listEntitiesByName = identityEntity.listEntitiesByName()
+        listEntitiesByName shouldContainExactlyInAnyOrder expectedListName
     }
 
     should("throw exception when merging non-existing source entity") {
@@ -257,7 +314,11 @@ class VaultIdentityEntityTest : ShouldSpec({
 
 })
 
-private suspend fun assertCreateEntity(identityEntity: VaultIdentityEntity, givenPath: String?, expectedReadPath: String) {
+private suspend fun assertCreateEntity(
+    identityEntity: VaultIdentityEntity,
+    givenPath: String?,
+    expectedReadPath: String
+) {
     assertCreateEntity(
         identityEntity,
         givenPath,
@@ -307,3 +368,57 @@ private suspend inline fun assertCreateEntity(
         response = actual,
     ) shouldBe actual
 }
+
+private suspend fun assertCreateEntityByName(
+    name: String,
+    identityEntity: VaultIdentityEntity, givenPath: String?, expectedReadPath: String
+) {
+    assertCreateEntityByName(
+        identityEntity,
+        givenPath,
+        expectedReadPath
+    ) { payload ->
+        identityEntity.createOrUpdateEntityByName(name)
+    }
+}
+
+private suspend fun assertCreateEntityByNameWithBuilder(
+    name: String,
+    identityEntity: VaultIdentityEntity,
+    givenPath: String?,
+    expectedReadPath: String
+) {
+    assertCreateEntityByName(
+        identityEntity,
+        givenPath,
+        expectedReadPath
+    ) { payload ->
+        identityEntity.createOrUpdateEntityByName(name) {
+            metadata = payload.metadata
+            policies = payload.policies
+            disabled = payload.disabled
+        }
+    }
+}
+
+private suspend inline fun assertCreateEntityByName(
+    identityEntity: VaultIdentityEntity,
+    givenPath: String?,
+    expectedReadPath: String,
+    createOrUpdate: (EntityCreateOrUpdateByNamePayload) -> EntityCreateResponse?
+) {
+    val given =
+        givenPath?.let { readJson<EntityCreateOrUpdateByNamePayload>(it) } ?: EntityCreateOrUpdateByNamePayload()
+    val result = createOrUpdate(given)
+
+    val id = result?.id
+    id shouldNotBe null
+
+    val actual = identityEntity.readEntityByID(id!!)
+
+    replaceTemplateString(
+        expected = readJson<EntityReadResponse>(expectedReadPath),
+        response = actual,
+    ) shouldBe actual
+}
+
